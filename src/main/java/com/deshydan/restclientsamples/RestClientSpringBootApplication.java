cf67d2a3ac71b95fdb2599d58a6f2c3f1eed0b5f
Deshy Dan <devdeshy@gmail.com>
2024-07-18 21:24:39 +0200

    Initial commit

---
+++ src/main/java/com/deshydan/restclientsamples/RestClientSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.deshydan.restclientsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestClientSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deshydan/restclientsamples/RestClientSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deshydan.restclientsamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
